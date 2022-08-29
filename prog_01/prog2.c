3fb01794a85411d3c995f325809f7a65c2cb7ba8
Pramod Singh Rautela <91174359+PramodSinghRautela@users.noreply.github.com>
2022-08-29 10:23:51 +0530

    Add files via upload

---
+++ prog_01/prog2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){ 
+  float num;
+  printf("Enter a number");
+  scanf("%f",&num);  
+
+  printf("Number round off to 2 decimal places is = %.3f\n",num);
+ 
+  
+  return 0;
+}
